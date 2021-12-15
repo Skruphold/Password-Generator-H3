44630cf84e182a628dff7c814b64223f888067c7
Skruphold <hunterco999@gmail.com>
2021-12-15 17:50:18 -0600

    5th commit

--- script.js
+++ script.js
@@ -4,12 +4,23 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword() {
 
   //Adding a variable array for numbers, special characters, uppercase and lower case characters. 
-  
   var numbersArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9",];
   var specialcharArray = ["+", "=", "_", "-", "(", ")", "*", "&", "^", "%", "$", "#", "@", "!"];
   var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  
+
+  // blank arrays to make it easier to call on them in prompts
+  var computerArray = [];
+  var userArray = [];
+
+  //prompts for the user.
+  uppercaseArray [1]
+    var charAmount = prompt ("Between 8 and 128 characters, how many would you like?");
+    var numerical = confirm ("Would you like your password to include numbers?");
+    var upper = confirm ( "Would you like your password to include Upper case letters?");
+    var lower = confirm (" Would you like your password to include any lower case letters?");
+    var specialChar = confirm ( "would you like your password to include any special characters?");
+
 }
 
 // Write password to the #password input
