58676569565b3f15e4888646044b5952cac4a0cf
Skruphold <hunterco999@gmail.com>
2021-12-15 18:23:30 -0600

    7th commit

--- script.js
+++ script.js
@@ -9,18 +9,29 @@ function generatePassword() {
   var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-  // blank arrays to make it easier to call on them in prompts
-  var computerArray = [];
+  // blank arrays to concat when I write my conditions for the prompt
+  var passArray = [];
   var userArray = [];
 
   //prompts for the user.
+  
   uppercaseArray [1]
-    var charAmount = prompt ("Between 8 and 128 characters, how many would you like?");
+    var passLength = prompt ("Between 8 and 128 characters, how many would you like?");
     var numerical = confirm ("Would you like your password to include numbers?");
     var upper = confirm ( "Would you like your password to include Upper case letters?");
-    var lower = confirm (" Would you like your password to include any lower case letters?");
-    var specialChar = confirm ( "would you like your password to include any special characters?");
+    var lower = confirm ("Would you like your password to include any lower case letters?");
+    var specialChar = confirm ("would you like your password to include any special characters?");
 
+  // condition for the array
+
+  if (!(passLength >=8 && passLength <=128)) {
+      alert("Please enter a value between 8 and 128!!!");
+  } else {
+    numerical = confirm("Would you like your password to include numbers?");
+    upper = confirm("Would you like your password to include Upper case letters");
+    lower = confirm("Would you like your password to include any lower case letters?");
+    specialChar = confirm("would you like your password to include any special characters?");
+  };  
 }
 
 // Write password to the #password input
